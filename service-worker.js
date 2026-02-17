daea9b8c3707b1f3c3cf9d64a3f78b93b7c2e540
Tim-vt22 <kruemel31112@gmail.com>
2026-02-18 00:34:18 +0100

    FORCE

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,16 @@
+const SW_VERSION = "v3";
+
 self.addEventListener("install", (event) => {
-  console.log("Service Worker installiert");
+  console.log("Service Worker installiert", SW_VERSION);
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("Service Worker aktiviert", SW_VERSION);
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(fetch(event.request));
 });
+
