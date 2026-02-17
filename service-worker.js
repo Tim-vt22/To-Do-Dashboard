ed9d6bd4b5912f0d67dbca829a3bb8e2ebcd31bb
Tim-vt22 <kruemel31112@gmail.com>
2026-02-18 00:42:47 +0100

    Fix iOS status bar properly

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,7 @@
-const SW_VERSION = "v3";
-
-self.addEventListener("install", (event) => {
-  console.log("Service Worker installiert", SW_VERSION);
+self.addEventListener("install", () => {
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
-  console.log("Service Worker aktiviert", SW_VERSION);
   event.waitUntil(self.clients.claim());
 });
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(fetch(event.request));
-});
-
